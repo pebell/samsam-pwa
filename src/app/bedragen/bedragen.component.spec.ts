a6edd2b3d4c4d75fe64653bab7addd7b500b2b14
Peter Ebell <pb.ebell@gmail.com>
2020-12-14 14:36:59 +0100

    angular core, cli, updated 10 => 11

--- src/app/bedragen/bedragen.component.spec.ts
+++ src/app/bedragen/bedragen.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BedragenComponent } from './bedragen.component';
 
@@ -6,7 +6,7 @@ describe('BedragenComponent', () => {
   let component: BedragenComponent;
   let fixture: ComponentFixture<BedragenComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BedragenComponent ]
     })
